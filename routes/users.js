5f957f3fd0a2157fae790e3560962ada8321245a
yxd <yuanxudong@meicai.cn>
2018-11-23 17:35:03 +0800

    add mongo

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+
+//mongodb
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+        
+var conn = mongoose.connect('mongodb://127.0.0.1:27017/blog');
+var User = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: String
+});
+
+var myModel = conn.model('user', User);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({title: 'aaaa', content: 'bbb'});
+  myModel.findOne({name:"zhangangs"}, function (err, user) {
+    res.json({title: 'Express', user: user });
+  });
 });
 
 module.exports = router;
