c8f4cf81d7db2e7d96ace6f3127445713b241da1
yxd <yuanxudong@meicai.cn>
2018-11-26 10:16:02 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,14 @@ var router = express.Router();
 //mongodb
 var mongoose = require('mongoose');
 //mongoose.Promise = global.Promise;
-        
-var conn = mongoose.connect('mongodb://127.0.0.1:27017/blog');
+// 连接数据库    
+var conn = mongoose.connect('mongodb://127.0.0.1:27017/blog', {useNewUrlParser:true}, function(err){
+    if(err){
+        console.warn('数据库连接失败：'+err);
+    }else {
+        console.log('数据库成功');
+    }
+});
 var User = new mongoose.Schema({
     name: String,
     email: String,
@@ -20,5 +26,9 @@ router.get('/', function(req, res, next) {
     res.json({title: 'Express', user: user });
   });
 });
+
+router.post('/add', function(req, res, next) {
+	myModel.col.insert(document)
+})
 
 module.exports = router;
